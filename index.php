8b3a9c3a44be8301ea38f2ff1e2247da428c9747
Chris Denmark <chrisdanish@googlemail.com>
2020-11-29 15:09:41 +0000

    Add background logos css

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>AOS: Battle Plan Generator</title>
+<link rel="stylesheet" type="text/css" href="css/bgcss.css" />
+<title>AOS: Battle Generator</title>
+<?php 
+include("generate.php");
+include("data.php");?>
 </head>
 <body>
 
-<h1>Battle Plan Generator</h1>
-<p>
-<?php 
-include("generate.php");
-include("data.php");
+<header class="main-header">
+    <h1><span class="logo"><img src="img/logo-aos-new.png" alt="Warhammer Age of Sigmar Warscroll Designer" /></span> Battle Generator</h1>
+  </header>
 
-generateGame($battle_plan, $realm);?>
+  <div class="hidden" style="font-family: 'Pompei Std'; display: none">preload</div>
+
+  <article class="main-description">
+    <p>The <strong>Warhammer Age of Sigmar - Battle Generator</strong> is a web application designed to allow you to create battles for use in the Warhammer Age of Sigmar game by Games Workshop Limited.</p>
+  </article>
+
+
+    </header>
+<p>
+
+
+<h3><?=generateGame($battle_plan, $realm);?></h3>
 </p>
 
 </body>
